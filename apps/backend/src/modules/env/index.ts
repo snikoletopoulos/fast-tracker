e09964da6610395cf0600aa1726c49f3e7083e91
Stavros <snikoletopoulos@hotmail.com>
2023-09-29 20:57:15 +0300

    chore: parse env variables on start up

---
+++ apps/backend/src/modules/env/index.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+	NODE_ENV: z.enum(["development", "production", "test"]),
+	CLERK_PUBLISHABLE_KEY: z.string().min(1),
+	CLERK_SECRET_KEY: z.string().min(1),
+});
+
+EnvSchema.parse(process.env);
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends z.infer<typeof EnvSchema> {}
+	}
+}
