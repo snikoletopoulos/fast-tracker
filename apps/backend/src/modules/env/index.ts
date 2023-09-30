11df627dfb18093f6318640fa28e9ff71738abdb
Stavros <snikoletopoulos@hotmail.com>
2023-09-30 22:12:52 +0300

    chore(env): remove unused env

--- apps/backend/src/modules/env/index.ts
+++ apps/backend/src/modules/env/index.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
 	NODE_ENV: z.enum(["development", "production", "test"]),
-	CLERK_PUBLISHABLE_KEY: z.string().min(1),
 	CLERK_SECRET_KEY: z.string().min(1),
 	CLERK_PEM_PUBLIC_KEY: z.string().min(1),
 });
