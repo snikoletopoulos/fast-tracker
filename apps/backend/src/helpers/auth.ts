92acce84d3ec3a5bfefd0074c0e4fb57b8e89cc1
Stavros <snikoletopoulos@hotmail.com>
2023-10-08 14:21:58 +0300

    chore: move modules to other folders

--- apps/backend/src/helpers/auth.ts
+++ apps/backend/src/helpers/auth.ts
@@ -1,11 +1,21 @@
 import { GraphQLError } from "graphql";
 import { type User } from "@clerk/clerk-sdk-node";
+import jwt from "jsonwebtoken";
 
-import { ApolloContext } from "index";
+import { type ApolloContext } from "index";
 
 export const allowAuthenticated = (
 	context: ApolloContext
 ): asserts context is ApolloContext & { user: User } => {
 	if (!context.user)
 		throw new GraphQLError("Not authenticated", { extensions: { code: 401 } });
+};
+
+export const parseJWT = (token: string) => {
+	if (!token) {
+		return null;
+	}
+
+	// eslint-disable-next-line import/no-named-as-default-member
+	return jwt.verify(token, process.env.CLERK_PEM_PUBLIC_KEY) as jwt.JwtPayload;
 };
--- apps/backend/src/modules/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const parseJWT = (token: string) => {
-	if (!token) {
-		return null;
-	}
-
-	return jwt.verify(token, process.env.CLERK_PEM_PUBLIC_KEY) as jwt.JwtPayload;
-};
